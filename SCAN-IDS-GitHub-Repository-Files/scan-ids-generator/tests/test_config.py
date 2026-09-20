f66fbde137ab52f0a94fc30a3f7d6e984b02197c
Dr Kambhampati Venkata Sambasiva Rao <kvsambasivarao65@gmail.com>
2026-09-21 04:41:27 +0530

    Delete SCAN-IDS-GitHub-Repository-Files/scan-ids-generator directory

--- SCAN-IDS-GitHub-Repository-Files/scan-ids-generator/tests/test_config.py
+++
@@ -1,5 +0,0 @@
-from src.config import ATTACK_CLASSES, FRAME_COUNTS, SEVERITY_MAP
-def test_taxonomy():
-    assert len(ATTACK_CLASSES) == 10
-    assert sum(FRAME_COUNTS.values()) == 500000
-    assert set(SEVERITY_MAP) == set(ATTACK_CLASSES)
